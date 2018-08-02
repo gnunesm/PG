9bb39f4c2efcc337521ef1a76d7dfd6d03a52a2e
gnunesm <gabnunes2112@gmail.com>
2018-08-02 12:02:43 -0300

    changed labels list items to type int

--- pickler.py
+++ pickler.py
@@ -23,8 +23,8 @@ for im in im_list:
     img = cv2.resize(img, (new_dim, new_dim), interpolation=cv2.INTER_CUBIC)
     dataset['data'].append(img)
     name = im.split('.')[0]
-    label = name.split('_')[-1]
+    label = int(name.split('_')[-1])
     dataset['labels'].append(label)
 
-with open('dataset.pkl', 'wb') as handle:
+with open('dataset_100_100.pkl', 'wb') as handle:
     pickle.dump(dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
